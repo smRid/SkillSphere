d71e8e7b22277582abafcc34d974ff433c1964c1
smRid <omorsk911@gmail.com>
2026-05-06 00:49:59 +0600

    add Better-Auth functions

---
+++ src/lib/auth-client.js
@@ -0,0 +1,17 @@
+"use client";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL:
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
+    (typeof window !== "undefined" ? window.location.origin : undefined),
+});
+
+export const {
+  useSession,
+  signIn,
+  signUp,
+  signOut,
+  updateUser,
+  getSession,
+} = authClient;
